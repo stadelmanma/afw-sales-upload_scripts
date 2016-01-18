cdee52ac546b3d18806442426fbbd4b7b2732a3b
stadelmanma <stadelmanma@gmail.com>
2016-01-18 17:06:47 -0500

    ar_ovchs reading completed
    ar ovchs file is now being successfully read and parsed into a
    dictionary

--- sales_parsing.py
+++ sales_parsing.py
@@ -25,6 +25,7 @@ import os
 #
 # stores an individual items information
 class sold_item:
+    #
     def __init__(self,input_str):
         # initializations (col_names will have something in it once Justin gets back with me)
         self.rep_id = 0
@@ -52,7 +53,8 @@ class sold_item:
 #
 # this stores the line data from the ar_ovchs report
 class ar_line:
-    def __init__(self,input_str,col_sizes):
+    #
+    def __init__(self,input_str,col_starts):
         # initializations
         self.rep_id = 0
         self.customer_id = 0
@@ -60,12 +62,28 @@ class ar_line:
         self.amount = 0.00
         self.col_dict = {}
         # processing input string
-        col_names = []
-
-
+        col_names = ['customer_id','name','desc','address','phone','rep_id','city','state','over','ar_ttl']
+        line = ''
+        for i in range(len(col_starts)-1):
+            col = (col_names[i] if i < len(col_names) else 'col-'+str(i))
+            self.col_dict[col] = input_str[col_starts[i]:col_starts[i+1]].strip()
+        i += 1
+        col = (col_names[i] if i < len(col_names) else 'col-'+str(i))
+        self.col_dict[col] = input_str[col_starts[-1]:].strip()
+        # updating variables
+        self.rep_id = self.col_dict['rep_id']
+        self.customer_id = self.col_dict['customer_id']
+        # checking for appended negative sign
+        if (re.search('-$',self.col_dict['over'])):
+            self.col_dict['over'] = '-'+re.sub('-$','',self.col_dict['over'])
+        self.amount = float(self.col_dict['over'])
+    #
+    def getCol(self,key):
+        return(self.col_dict[key])
 #
 # stores customer information and totals 
 class customer:
+    #
     def __init__(self,customer_id):
         self.id = customer_id
         self.daily_sales = {}
@@ -79,7 +97,6 @@ class customer:
         except KeyError:
             self.daily_sales[date]  = price
             self.daily_costs[date]  = cost
-
 #
 #
 ################################################################################
@@ -103,8 +120,6 @@ def read_ec_order_upload(infile,sold_items_list):
     for i in range(len(content_arr)):
         item = sold_item(content_arr[i])
         sold_items_list.append(item)
-    #
-    return
 #
 # this totals each customers sales
 def customer_totals(customer_sales_dict,sold_items_list):
@@ -129,6 +144,7 @@ def process_ec_order_upload(ec_infile,sold_items_list,customer_sales_dict):
 #
 # ### ### ar_ovchs File Processing ### ### #
 #
+#
 # this function reads the ar_ovchs file 
 def read_ar_ovchs(ar_infile,customer_ar_dict):
     # reading infile
@@ -140,25 +156,55 @@ def read_ar_ovchs(ar_infile,customer_ar_dict):
     content_arr = re.split(r'\n',content)
     content_arr = list(filter(None,content_arr))
     #
+    # getting date from first header
+    date = re.split('\s{2,}',content_arr[1])[2]
+    date = date.strip()
+    #
     # filtering uneeded lines
     i = 0
+    max_len = 0
     while i < len(content_arr):
         line = content_arr[i]
         #
         if not (re.match(r'\s{1,6}[A-Z0-9]+\s',line)):
             del content_arr[i]
         else:
+            max_len = (len(line) if (len(line) > max_len) else max_len)
+            print(line)
             i += 1
-    print(len(content_arr));
     #
-    # determining column sizes
-    
+    # determining column sizes (semi-brute force method)
+    column_starts = get_column_starts(content_arr,6,max_len)
     #
-    # I'll eventually need a regex like to to process everything re.match(r'(.{15})(.{15})(.{15})',s)
-    # where the 15s will be a variable and s is the line to process
-    # this part might be handled by the class object
+    # creating ar_line objects
+    for line in content_arr:
+        ar = ar_line(line,column_starts)
+        ar.date = date
+        customer_ar_dict[ar.customer_id] = ar
+#
+# this function determines the column starts of fixed width reports
+def get_column_starts(content_arr,start_index,max_len):
+    # initializing variables
+    column_starts = [0]
+    s = start_index
+    while True:
+        # inital testing 
+        for line in content_arr:
+            if (not re.match('\s',line[s-1:s])):
+                s += 1
+                break
+        # advancing s until failure
+        if (line == content_arr[-1]):
+            s += 1
+            for line in content_arr:
+                if (not re.match('\s',line[s-1:s])):
+                    column_starts.append(s-1)
+                    break
+        #
+        if (s > max_len):
+            break
     #
-    
+    return(column_starts)
 #
 #
 ################################################################################
@@ -170,13 +216,18 @@ def read_ar_ovchs(ar_infile,customer_ar_dict):
 ec_infile = '../EC-Order-Upload.txt'
 ar_infile = '../ar_ovchs.txt'
 sold_items_list = []
+ar_lines_list = []
 customer_sales_dict = {}
 customer_ar_dict = {}
+rep_totals_dict = {}
 #
 # processing the EC upload file
+#!!!!!!!!! I NEED TO CHECK FOR NEG SIGN ON BACK END !!!!!!!!!!
 process_ec_order_upload(ec_infile,sold_items_list,customer_sales_dict)
 #
 # processing the ar_ovchs file
 read_ar_ovchs(ar_infile,customer_ar_dict)
+
+
 
 
