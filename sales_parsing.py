c1342bacfb764db621ecf0478d3711eae321e0ac
stadelmanma <stadelmanma@gmail.com>
2016-01-18 20:02:00 -0500

    s_wkcomm parsing complete
    All inputs files are being parsed. Next stage is creating the SQL
    statements

--- sales_parsing.py
+++ sales_parsing.py
@@ -61,15 +61,16 @@ class ar_line:
         self.date = ''
         self.amount = 0.00
         self.col_dict = {}
+        #
         # processing input string
         col_names = ['customer_id','name','desc','address','phone','rep_id','city','state','over','ar_ttl']
-        line = ''
         for i in range(len(col_starts)-1):
             col = (col_names[i] if i < len(col_names) else 'col-'+str(i))
             self.col_dict[col] = input_str[col_starts[i]:col_starts[i+1]].strip()
         i += 1
         col = (col_names[i] if i < len(col_names) else 'col-'+str(i))
         self.col_dict[col] = input_str[col_starts[-1]:].strip()
+        #
         # updating variables
         self.rep_id = self.col_dict['rep_id']
         self.customer_id = self.col_dict['customer_id']
@@ -81,6 +82,35 @@ class ar_line:
     def getCol(self,key):
         return(self.col_dict[key])
 #
+# this stores the line data from the ar_ovchs report
+class com_line:
+    #
+    def __init__(self,input_str,col_starts):
+        # initializations
+        self.rep_id = 0
+        self.date = ''
+        self.credits = 0.00
+        self.col_dict = {}
+        #
+        # processing input string
+        col_names = ['spacer','rep_id','rep_name','sales','credits','cost','profit','margin','comm','perc']
+        for i in range(len(col_starts)-1):
+            col = (col_names[i] if i < len(col_names) else 'col-'+str(i))
+            self.col_dict[col] = input_str[col_starts[i]:col_starts[i+1]].strip()
+        i += 1
+        col = (col_names[i] if i < len(col_names) else 'col-'+str(i))
+        self.col_dict[col] = input_str[col_starts[-1]:].strip()
+        #
+        # updating variables
+        self.rep_id = self.col_dict['rep_id']
+        # checking for appended negative sign
+        if (re.search('-$',self.col_dict['credits'])):
+            self.col_dict['credits'] = '-'+re.sub('-$','',self.col_dict['credits'])
+        self.credits = float(self.col_dict['credits'])
+    #
+    def getCol(self,key):
+        return(self.col_dict[key])
+#
 # stores customer information and totals 
 class customer:
     #
@@ -88,6 +118,23 @@ class customer:
         self.id = customer_id
         self.daily_sales = {}
         self.daily_costs = {}
+    #
+    def incrementSales(self,date,price,cost):
+        try:
+            self.daily_sales[date] += price
+            self.daily_costs[date] += cost
+        except KeyError:
+            self.daily_sales[date]  = price
+            self.daily_costs[date]  = cost
+#
+# stores sales rep information and totals 
+class sales_rep:
+    #
+    def __init__(self,rep_id):
+        self.id = rep_id
+        self.daily_sales  = {}
+        self.daily_costs  = {}
+        self.credit_total = {}
         self.ar_total = {}
     #
     def incrementSales(self,date,price,cost):
@@ -97,6 +144,18 @@ class customer:
         except KeyError:
             self.daily_sales[date]  = price
             self.daily_costs[date]  = cost
+    #
+    def incrementAr(self,date,amount):
+        try:
+            self.ar_total[date] += amount
+        except KeyError:
+            self.ar_total[date]  = amount
+    #
+    def incrementCredits(self,date,amount):
+        try:
+            self.credit_total[date] += amount
+        except KeyError:
+            self.credit_total[date]  = amount
 #
 #
 ################################################################################
@@ -170,7 +229,6 @@ def read_ar_ovchs(ar_infile,customer_ar_dict):
             del content_arr[i]
         else:
             max_len = (len(line) if (len(line) > max_len) else max_len)
-            print(line)
             i += 1
     #
     # determining column sizes (semi-brute force method)
@@ -206,6 +264,88 @@ def get_column_starts(content_arr,start_index,max_len):
     #
     return(column_starts)
 #
+# this function totals up AR by rep
+def rep_ar_totals(customer_ar_dict,rep_totals_dict):
+    #
+    for cust in customer_ar_dict:
+        cust = customer_ar_dict[cust]
+        try:
+            rep = rep_totals_dict[cust.rep_id]
+            rep.incrementAr(cust.date,cust.amount)
+        except KeyError:
+            rep = sales_rep(cust.rep_id)
+            rep.incrementAr(cust.date,cust.amount)
+            rep_totals_dict[cust.rep_id] = rep
+#
+# this just acts as a driver function to handle all processing of ar_ovchs file
+def process_ar_ovchs_file(ec_infile,customer_ar_dict,rep_totals_dict):
+    #
+    # reading EC upload file
+    read_ar_ovchs(ar_infile,customer_ar_dict)
+    #
+    # totaling customer ar into rep
+    rep_ar_totals(customer_ar_dict,rep_totals_dict)
+#
+#
+# ### ### ar_ovchs File Processing ### ### #
+#
+#
+# this function reads the s_wkcomm file
+def read_s_wkcomm(com_infile,com_line_list):
+    # reading infile
+    com_file = open(com_infile,'r')
+    content = com_file.read()
+    com_file.close()
+    #
+    # splitting by form feed characters (\x0c)
+    rep_page = re.split(r'\x0c',content)[1]
+    #
+    # splitting rep page by newlines
+    line_arr = rep_page.split('\n')
+    line_arr = list(filter(None,line_arr))
+    #
+    # getting date from header
+    date = re.split('\s{2,}',line_arr[1])[2]
+    date = date.strip()
+    # removing header and totals lines
+    del line_arr[0:6]
+    del line_arr[-1]
+    del line_arr[-1]
+    #
+    column_starts = get_column_starts(line_arr,24,len(line_arr[0])+5)
+    print (column_starts)
+    #
+    for line in line_arr:
+        cl = com_line(line,column_starts)
+        cl.date = date
+        com_line_list.append(cl)
+#
+# this function adds the credits line to the rep object
+def rep_credit_totals(com_line_list,rep_totals_dict):
+    #
+    for cl in com_line_list:
+        try:
+            rep = rep_totals_dict[cl.rep_id]
+            rep.incrementCredits(cl.date,cl.credits)
+        except KeyError:
+            rep = sales_rep(cl.rep_id)
+            rep.incrementCredits(cl.date,cl.credits)
+            rep_totals_dict[cl.rep_id] = rep
+    #
+    keys = list(rep_totals_dict.keys())
+    keys.sort()
+    for key in keys:
+        print(rep_totals_dict[key].id,rep_totals_dict[key].credit_total,rep_totals_dict[key].ar_total)
+#
+# this just acts as a driver function to handle all processing of s_wkcomm file
+def process_s_wkcomm_file(com_infile,com_line_list,rep_totals_dict):
+    #
+    # reading EC upload file
+    read_s_wkcomm(com_infile,com_line_list)
+    #
+    # totaling customer ar into rep
+    rep_credit_totals(com_line_list,rep_totals_dict)
+#
 #
 ################################################################################
 ############################# Program Execution ################################
@@ -215,19 +355,24 @@ def get_column_starts(content_arr,start_index,max_len):
 # initializations
 ec_infile = '../EC-Order-Upload.txt'
 ar_infile = '../ar_ovchs.txt'
+com_infile = '../s_wkcomm.txt'
 sold_items_list = []
-ar_lines_list = []
+com_line_list = []
 customer_sales_dict = {}
 customer_ar_dict = {}
 rep_totals_dict = {}
+## need to make a script to locate files, the target outputs will have the same name but different numeric extension
+## need to create a log to handle all errors and then have that emailed to justin ##
+## might be able to do this through a batch process + python 
 #
 # processing the EC upload file
-#!!!!!!!!! I NEED TO CHECK FOR NEG SIGN ON BACK END !!!!!!!!!!
 process_ec_order_upload(ec_infile,sold_items_list,customer_sales_dict)
 #
 # processing the ar_ovchs file
-read_ar_ovchs(ar_infile,customer_ar_dict)
-
+process_ar_ovchs_file(ec_infile,customer_ar_dict,rep_totals_dict)
+#
+# processing the s_wkcomm file
+process_s_wkcomm_file(com_infile,com_line_list,rep_totals_dict)
 
 
 
